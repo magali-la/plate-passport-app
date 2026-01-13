3631a09d9419181ece358378c8c0b6531dc09136
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-13 09:42:28 -0500

    feat(recipe remix): import custom hook for random meal generation, inject meal object values in UI, add conditional UI based on loading state, add useEffect to mount data on load, add  onclick event to retrieve new random meal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,47 @@
+import { useEffect } from "react";
+import useFetchRandom from "../hooks/useFetch"
+
 export default function Home() {
+    // import with custom fetch random custom hook
+    const { meal, loading, error, displayRandomMeal } = useFetchRandom();
+
+    // use effect to load the data on mount - if the function is called again, restart the process
+    useEffect(() => {
+        // call function to set states for this fetch 
+        displayRandomMeal();
+
+    }, []);
 
     return (
         <>
-            <h1>Plate Passport Home</h1>
-            <section className=" flex flex-row justify-between" aria-label="Discover a random meal">
-                {/* recipe image */}
-                <div></div>
-                {/* recipe details */}
-                <div>
-                    <h3>Recipe Title</h3>
-                    <p>Recipe Ingredients</p>
-                    <p>Recipe Insctuctions</p>
-                    <button>View Recipe</button>
-                </div>
+            <h1>Discover the best foods from all around the world!</h1>
+            <section className="flex justify-center" aria-label="Discover a random meal">
+                {/* recipe details - conditional loading text vs actual recipe */}
+                {loading ? (
+                    <div>
+                        <p>Generating your meal of the day...</p>
+                        {/* todo - loading spinner */}
+                    </div>
+                ) : (
+                    // once the loading is set to false when the data is successfully set to state, then use meal object
+                    <div className="flex flex-row gap-20 px-30">
+                        {/* thumbnail of meal */}
+                        <div className="flex rounded-2xl overflow-clip">
+                            <img src={`${meal?.strMealThumb}/medium`} alt={`image of ${meal?.strMeal}`} className="h-full w-full" />
+                        </div>
+                        {/* recipe info and remix button */}
+                        <div className="flex flex-col gap-4">
+                            <h3 className="text-2xl font-semibold">{meal?.strMeal}</h3>
+                            {meal?.strArea && <p className="italic">{meal?.strArea}</p>}
+                            {meal?.strTags && <p>Tags: {meal.strTags}</p>}
+                            <p>Category: {meal?.strCategory}</p>
+                            <button>View Recipe</button>
+                            <p className="">Want to load a new recipe?</p>
+                            <button onClick={displayRandomMeal}>Recipe Remix</button>
+                        </div>
+                    </div>
+                )}
             </section>
-            <p className="">Want to load a new recipe?</p>
-            <button>Recipe Remix</button>
         </>
     )
 }
