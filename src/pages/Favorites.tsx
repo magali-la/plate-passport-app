908ceff572211b522cdd6ddb4ee41e13a10c936e
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-15 21:01:53 -0500

    feat(search): add search functionality on favorites page with empty state handling

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -9,6 +9,8 @@ export default function Favorites() {
     const [favoriteMealIds] = useState<string[]>(() => getFavorites());
     // set state for array full of full meal objects
     const [favoriteMeals, setFavoriteMeals] = useState<Meal[]>([]);
+    // set state for search input
+    const [searchTerm, setSearchTerm] = useState<string>('');
     // use display meal only to get the full meal data for the card to work
     const { displayMeal } = useFetchMealById();
     
@@ -53,15 +55,38 @@ export default function Favorites() {
         )
     }
 
+    // set handlers for search
+    function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchTerm(event.target.value)
+    }
+
+    // lastly base the map off of filtered meals which will depend on the current search term for live search for category, name, or cuisine
+    const filteredMeals = favoriteMeals.filter((meal) => 
+        meal.strMeal.toLowerCase().includes(searchTerm.toLowerCase()) || 
+        meal.strCategory.toLowerCase().includes(searchTerm.toLowerCase()) || 
+        meal.strArea.toLowerCase().includes(searchTerm.toLowerCase()));
+
     return (
-        <>
+        <div className="flex flex-col">
             <h1>Your Plate Passport</h1>
             <p>Meals you've favorited:</p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-6">
-                {favoriteMeals.map((meal) => (
-                    <RecipeCard key={meal.idMeal} meal={meal} />
-                ))}
+            {/* search section */}
+            <div className="flex flex-col self-end w-fit">
+                <label>Search your favorites recipes:</label>
+                <input type="text" className="border-2 border-blue-400 placeholder:text-blue-950 p-3 w-80" value={searchTerm} onChange={handleSearch} placeholder="Search by name, category, or cuisine"/>
             </div>
-        </>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-6">
+                {/* set the array to the search array - ternary for invalid search results or map the results */}
+                {filteredMeals.length === 0 ? (
+                    <h3>No meals found for '{searchTerm}'. Try another search!</h3>
+                    ) : ( 
+                        filteredMeals.map((meal) => (
+                            <RecipeCard key={meal.idMeal} meal={meal} />
+                        ))
+                    )
+                }
+            </div>
+        </div>
     )
 }
